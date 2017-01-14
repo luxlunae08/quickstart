b300d497fdd7e4c56cf79c4c53f6489859d6f88d
bahara-PC\bahara <luxlunae08@gmail.com>
2017-01-14 18:34:31 +0900

    Hero class 추가

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id : number;
+  name : string;
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>{{title}}</h1><h2>{{name}} details!</h1>`,
+  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`,
 })
 export class AppComponent  { 
 	title = 'Tour of heroes'; 
