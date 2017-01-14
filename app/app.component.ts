6654d698404354a4e22a4b384c1b54ab396e9968
bahara-PC\bahara <luxlunae08@gmail.com>
2017-01-14 21:09:38 +0900

    master-detail

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,6 @@ export class Hero {
 	  name : string;
 }
 
-
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
   { id: 12, name: 'Narco' },
@@ -25,15 +24,19 @@ const HEROES: Hero[] = [
 	<h1>{{title}}</h1>
 	<h2>My Heroes</h2>
 	<ul class="heroes">
-		<li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+		<li *ngFor="let hero of heroes" 
+		 [class.selected]="hero === selectedHero"
+		 (click)="onSelect(hero)">
 		  <span class="badge">{{hero.id}}</span> {{hero.name}}
 		</li>		
 	</ul>
-	<h2>{{hero.name}} details!</h2>
-		<div><label>id: </label>{{hero.id}}</div>
-		<div>
-		<label>name: </label>
-		<input [(ngModel)]="hero.name" placeholder="name">
+	<div *ngIf="selectedHero">
+		<h2>{{selectedHero.name}} details!</h2>
+			<div><label>id: </label>{{selectedHero.id}}</div>
+			<div>
+			<label>name: </label>
+			<input [(ngModel)]="selectedHero.name" placeholder="name">
+		</div>
 	</div>
 	  `,
 	styles: [`
@@ -89,11 +92,12 @@ const HEROES: Hero[] = [
 })
 
 export class AppComponent  { 
-	title = 'Tour of heroes'; 
-	hero: Hero = {
-	  id: 1,
-	  name: 'Windstorm'
-	};
-
+	title = 'Tour of heroes'; 	
 	heroes = HEROES;
+	selectedHero: Hero;
+
+	onSelect(hero: Hero): void {
+	  this.selectedHero = hero;
+	}
 }
+
